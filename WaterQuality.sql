0d1dcbc20a75a9a4aaba469f6873048afe689251
Justin Papreck <justin.papreck@gmail.com>
2023-03-01 18:37:00 -0800

    Recreated files for DB, updated cleaning files, and wrote instructions for using CTEs in a jupyter file

--- WaterQuality.sql
+++ WaterQuality.sql
@@ -1,10 +1,47 @@
--- CREATE DATABASE WaterQuality;
+CREATE DATABASE WaterQuality;
 
-DROP TABLE period_of_record;
-DROP TABLE field_results;
-DROP TABLE lab_results;
+-- DROP TABLE dbo.lab_results
+CREATE TABLE lab_results (
+    station_id INT, 
+    station_name VARCHAR(500), 
+    full_station_name VARCHAR(500), 
+    station_number VARCHAR(250), 
+    station_type VARCHAR(250),
+    latitude FLOAT,
+    longitude FLOAT,
+    status_ VARCHAR(250),
+    county_name VARCHAR(250),
+    sample_code VARCHAR(250),
+    sample_date DATETIME,
+    sample_depth VARCHAR(50),
+    sample_depth_units VARCHAR(250),
+    parameter VARCHAR(250), 
+    result FLOAT,
+    reporting_limit FLOAT,
+    units VARCHAR(250),
+    method_name VARCHAR(250)
+);
 
-CREATE TABLE IF NOT EXISTS period_of_record (
+SELECT * 
+FROM dbo.lab_results;
+
+BULK INSERT dbo.lab_results
+FROM "C:\Users\justi\OneDrive\Desktop\Analytics\Water_Quality\Data\lab_results.csv"
+WITH 
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2
+)
+GO
+;
+
+-- Verify that these rows match
+SELECT COUNT(station_id) AS Station_ID, COUNT(latitude) AS Lat, COUNT(longitude) AS Lon
+FROM dbo.lab_results;
+-- There are more station ids than coordinates, but the lat/long match
+
+--DROP TABLE period_of_record;
+CREATE TABLE period_of_record (
     station_id INT, 
     station_name VARCHAR(250), 
     full_station_name VARCHAR(250), 
@@ -15,80 +52,117 @@ CREATE TABLE IF NOT EXISTS period_of_record (
     county_name VARCHAR(250),
     parameter VARCHAR(250),
     sample_count INT,
-    sample_date_min TIMESTAMP,
-    sample_date_max TIMESTAMP
+    sample_date_min DATETIME,
+    sample_date_max DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS field_results (
-    station_id INT, 
-    station_name VARCHAR,
-    station_number VARCHAR,
-    full_station_name VARCHAR, 
-    station_type VARCHAR,
-    latitude FLOAT,
-    longitude FLOAT,
-    status_ VARCHAR,
-    county_name VARCHAR,
-    sample_code VARCHAR,
-    sample_date TIMESTAMP,
-    sample_depth FLOAT,
-    sample_depth_units VARCHAR,
-    anl_data_type VARCHAR,
-    parameter VARCHAR, 
-    fdr_result FLOAT,
-    fdr_text_result VARCHAR,
-    fdr_date_result VARCHAR,
-    fdr_reporting_limit FLOAT, 
-    uns_name VARCHAR(50),
-    mth_name VARCHAR,
-    fdr_footnote VARCHAR
-);
-
-
-
-CREATE TABLE IF NOT EXISTS field_results ( 
-    station_id INT,
-    station_name VARCHAR,
-    station_number VARCHAR,
-    full_station_name VARCHAR,
-    station_type VARCHAR,
-    latitude FLOAT,
-    longitude FLOAT,
-    status_ VARCHAR,
-    county_name VARCHAR,
-    sample_code VARCHAR,
-    sample_date TIMESTAMP,
-    sample_depth FLOAT,
-    sample_depth_units VARCHAR,
-    anl_data_type VARCHAR,
-    parameter VARCHAR,
-    fdr_result FLOAT,
-    fdr_text_result VARCHAR,
-    fdr_date_result VARCHAR,
-    fdr_reporting_limit FLOAT,
-    uns_name VARCHAR,
-    mth_name VARCHAR,
-    fdr_footnote VARCHAR
+BULK INSERT dbo.period_of_record
+FROM "C:\Users\justi\OneDrive\Desktop\Analytics\Water_Quality\Data\period_of_record.csv"
+WITH 
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2
 )
+GO
+;
 
+SELECT COUNT(station_id) AS Station_ID, COUNT(latitude) AS Lat, COUNT(longitude) AS Lon
+FROM dbo.period_of_record
+-- Again, a few hundred station IDs don't have coordinates
 
-CREATE TABLE IF NOT EXISTS lab_results (
+--DROP TABLE dbo.field_results;
+
+CREATE TABLE field_results (
     station_id INT, 
-    station_name VARCHAR(250), 
-    full_station_name VARCHAR(250), 
-    station_number VARCHAR(250), 
+    station_name VARCHAR(500),
+    station_number VARCHAR(250),
+    full_station_name VARCHAR(500), 
     station_type VARCHAR(250),
     latitude FLOAT,
     longitude FLOAT,
     status_ VARCHAR(250),
     county_name VARCHAR(250),
     sample_code VARCHAR(250),
-    sample_date TIMESTAMP,
-    sample_depth VARCHAR(25),
+    sample_date DATETIME,
+    sample_depth VARCHAR(50),
     sample_depth_units VARCHAR(250),
-    parameter VARCHAR(250), 
-    result FLOAT,
-    reporting_limit FLOAT,
-    units VARCHAR(250),
-    method_name VARCHAR(250)
+    anl_data_type VARCHAR(50),
+    parameter VARCHAR(500), 
+    fdr_result FLOAT,
+    fdr_text_result VARCHAR(500),
+    fdr_date_result VARCHAR(250),
+    fdr_reporting_limit FLOAT, 
+    uns_name VARCHAR(50),
+    mth_name VARCHAR(50),
+    fdr_footnote VARCHAR(MAX)
 );
+
+BULK INSERT dbo.field_results
+FROM "C:\Users\justi\OneDrive\Desktop\Analytics\Water_Quality\Data\field_results.csv"
+WITH 
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2
+)
+GO
+;
+
+SELECT COUNT(station_id) AS Station_ID, COUNT(latitude) AS Lat, COUNT(longitude) AS Lon
+FROM dbo.field_results
+-- There are 30 station IDs without coordinates
+
+-- DROP TABLE dbo.stations;
+CREATE TABLE stations (
+    station_id INT, 
+    station_name VARCHAR(500),
+    full_station_name VARCHAR(500), 
+    station_number VARCHAR(250),
+    station_type VARCHAR(250),
+    latitude FLOAT,
+    longitude FLOAT,
+    county_name VARCHAR(250),
+    sample_count INT,
+    sample_date_min DATETIME,
+    sample_date_max DATETIME
+);
+
+
+BULK INSERT dbo.stations
+FROM "C:\Users\justi\OneDrive\Desktop\Analytics\Water_Quality\Data\stations.csv"
+WITH 
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2
+)
+GO
+;
+
+SELECT COUNT(station_id) AS Station_ID, COUNT(latitude) AS Lat, COUNT(longitude) AS Lon
+FROM dbo.stations
+-- Same situation as before
+
+-- DROP TABLE dbo.state_regulations
+CREATE TABLE state_regulations (
+	contaminant VARCHAR(250) NOT NULL, 
+	state_max_level FLOAT, 
+	state_detection_limit FLOAT, 
+	state_health_goal FLOAT, 
+	state_health_date INT, 
+	federal_max_level FLOAT,
+	federal_max_level_goal FLOAT, 
+	units VARCHAR(50)
+);
+
+BULK INSERT dbo.state_regulations
+FROM "C:\Users\justi\OneDrive\Desktop\Analytics\Water_Quality\Data\state_regulations.csv"
+WITH 
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2
+)
+GO
+;
+
+SELECT *
+FROM dbo.state_regulations
+;
